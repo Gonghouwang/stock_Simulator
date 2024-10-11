2d3122e1a7ab1e008ccdcfa9cd64ba084a39a821
yks1 <1417044093@qq.com>
2024-10-11 12:26:19 +0800

    修复一些bug

--- src/components/navmenu.vue
+++ src/components/navmenu.vue
@@ -5,20 +5,21 @@
              mode="horizontal"
              style="font-weight: bold; font-size: 20px;"
              >
-      <el-col span="8" style="display: flex; justify-content: flex-start; ">
+      <el-col span="8" style="display: flex; justify-content: flex-start; align-items: center; ">
         <el-menu-item index="/" style="margin-left: 2vw">home</el-menu-item>
         <el-menu-item index="/news">news</el-menu-item>
       </el-col>
       <el-col span="8" style="display: flex; justify-content: center; ">
-        <div style="padding: 0; height: 56px;">
-          <img src="../assets/logo.svg" alt="logo" style="padding-top: 10px; color: #409EFF">
+        <div>
+          <img src="../assets/logo.svg" alt="logo" style="padding-top: 12px; color: #409EFF">
         </div>
+        <span style="padding-top: 14px; padding-left: 10px; display: flex">SmartStockSim</span>
       </el-col>
       <el-col span="8" style="display: flex; justify-content: flex-end; ">
         <div class="button-container">
-          <span v-if="this.$store.isLogged"> Hi! {{ this.$store.username }} </span>
+          <span v-if="this.$store.state.isLogged" class="username">Hi! {{ this.$store.state.username }} </span>
 
-          <button class="Btn" v-if="this.$store.isLogged" @click="logout()">
+          <button class="Btn" v-if="this.$store.state.isLogged" @click="logout()">
             <div class="sign">
               <svg viewBox="0 0 512 512">
                 <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
@@ -27,7 +28,7 @@
             <div class="text">Logout</div>
           </button>
 
-          <button class="Btn" v-if="!this.$store.isLogged" @click="goLogin()">
+          <button class="Btn" v-if="!this.$store.state.isLogged" @click="goLogin()">
             <div class="sign">
               <svg viewBox="0 0 512 512">
                 <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path>
@@ -91,6 +92,12 @@ export default {
 
 .icon {
   font-size: 20px;
+}
+
+.username{
+  font-size: 16px;
+  font-weight: normal;
+  margin-right: 25px;
 }
 
 .Btn {
